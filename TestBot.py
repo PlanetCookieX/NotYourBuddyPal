c7dd1baf789a1fd10214577bd112727d68c27570
PlanetCookieX <PlanetCookieX@gmail.com>
2015-06-06 11:49:51 -0400

    First commit

---
+++ TestBot.py
@@ -0,0 +1,64 @@
+import praw
+import time
+
+r = praw.Reddit(user_agent = "An API usage that replies to comments containing the words buddy, pal, guy, or friend and replys accordingly /u/NotYourBuddyBot")
+
+r.login();
+
+words_to_match = ['buddy','pal','guy', 'friend']
+cache = []
+
+
+def run_bot():
+    print("Running Bot...")
+    subreddit = r.get_subreddit("test")
+    comments = subreddit.get_comments(limit=100)
+    print("Fetching Comments...")
+    for comment in comments:
+
+        print("Testing Comment " +comment.id)
+
+        comment_text = comment.body.lower()
+
+        isBuddy = False
+        isPal = False
+        isFriend = False
+        isGuy = False
+
+        if "buddy" in comment_text:
+            isBuddy = True
+        elif "pal" in comment_text:
+            isPal = True
+        elif "friend" in comment_text:
+            isFriend = True
+        elif "guy" in comment_text:
+            isGuy = True
+
+        if comment.id not in cache and isBuddy:
+            message = "I'm not your buddy pal!"
+            comment.reply(message)
+            print("Replying To Comment " +comment.id +"with" +message)
+            cache.append(comment.id)
+
+        elif comment.id not in cache and isPal:
+            message = "I'm not your pal friend!"
+            comment.reply(message)
+            print("Replying To Comment " +comment.id +"with" +message)
+            cache.append(comment.id)
+
+        elif comment.id not in cache and isFriend:
+            message = "I'm not yoru friend guy!"
+            comment.reply(message)
+            print("Replying To Comment " +comment.id +"with" +message)
+            cache.append(comment.id)
+
+        elif comment.id not in cache and isGuy:
+            message = "I'm not your guy buddy!"
+            comment.reply(message)
+            print("Replying To Comment " +comment.id +"with" +message)
+            cache.append(comment.id)
+
+
+while True:
+    run_bot()
+    time.sleep(10)
